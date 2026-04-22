7df328f4a3502a24c788075fdfd103ee7fe84d43
unknown <gibsonsangk@gmail.com>
2026-04-22 16:18:14 +0300

    status backend

--- src/models/status.js
+++ src/models/status.js
@@ -1,24 +1,32 @@
-// models/status.js
-const mongoose = require("mongoose");
-
 const statusSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
     lastName: { type: String },
     firstName: { type: String },
     nickname: { type: String },
+
     caption: { type: String },
+
+    viewed: {
+      type: Boolean,
+      default: false,
+    },
+
     media: [{ type: String }],
     likes: [{ type: String }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+
     backgroundColor: {
       type: String,
       default: "#1e293b",
     },
+
+    // ✅ TTL FIELD
+    expiresAt: {
+      type: Date,
+      default: Date.now,
+      expires: 60 * 60 * 24, // 24 hours in seconds
+    },
   },
   { timestamps: true },
 );
-
-const Status = mongoose.models.Status || mongoose.model("Status", statusSchema);
-
-module.exports = Status;
